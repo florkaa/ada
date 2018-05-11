b6fc6879321d9b40e0a115ef2c73db8b79de1d9d
florka <[florencia.ayc@gmail.com]>
2018-05-11 15:08:10 -0300

    jquery p.o.o

--- ejercicios/35_personaPOO/js/index.js
+++ ejercicios/35_personaPOO/js/index.js
@@ -1,22 +1,31 @@
-let alumno = new Alumno('Pepe', 'Feliz', '12-07-1999', '43509989', '1004');
+$('#submit').on('click', e=>{
+	e.preventDefault();
+	var name = $('#name').val();
+	var lastName = $('#lastName').val();
+	var bday = $('#bday').val();
+	var dni = $('#dni').val(); 
+	var legajo = $('#legajo').val();
 
-let materia = new Materia('física', 'lun y mié 19:00 a 22:00');
-materia.addNota(9);
-materia.addNota(5);
+	let alumno = new Alumno(name, lastName, bday, dni, legajo);
 
-let materia2 = new Materia('química', 'mar y vie 18:00 a 21:00');
-materia2.addNota(7);
-materia2.addNota(8);
+	let materia = new Materia('física', 'lun y mié 19:00 a 22:00');
+	materia.addNota(9);
+	materia.addNota(5);
 
-alumno.addMateria(materia);
-alumno.addMateria(materia2);
+	let materia2 = new Materia('química', 'mar y vie 18:00 a 21:00');
+	materia2.addNota(7);
+	materia2.addNota(8);
 
-let promedioMateria = materia.calcularPromedio();
-let promedioMateria2 = materia2.calcularPromedio();
+	alumno.addMateria(materia);
+	alumno.addMateria(materia2);
 
-let promAlumno = alumno.calcularPromedioGral();
+	let promedioMateria = materia.calcularPromedio();
+	let promedioMateria2 = materia2.calcularPromedio();
 
-console.log(alumno)
-console.log(materia, materia2);
-console.log('promedio física:', promedioMateria, 'promedio química', promedioMateria2);
-console.log(promAlumno)
+	let promAlumno = alumno.calcularPromedioGral();
+
+	console.log(alumno)
+	console.log(materia, materia2);
+	console.log('promedio física:', promedioMateria, 'promedio química', promedioMateria2);
+	console.log(promAlumno)
+});
