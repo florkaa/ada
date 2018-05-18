3dacd26a2b6112dd5d4200c58aa2fddf6543bf7e
florkaa <florencia.ayc@gmail.com>
2018-05-17 23:28:33 -0300

    tp2 memotest final

--- trabajos_practicos/TP2Memotest/js/index.js
+++ trabajos_practicos/TP2Memotest/js/index.js
@@ -1,56 +1,84 @@
-
-// 4- Permitir ingresar el nombre del jugador al iniciar el juego [x]
-
-function saludo() {
+function welcome() {
     var txt;
-    var person = prompt("Escribí tu nombre:");
-    if (person == null || person == "") {
-        txt = "Canceló.";
-    } else {
+    var person = prompt("Escribí tu nombre para comenzar a jugar:");
+    if(person == null || person == "") {
+        txt = "Buu, no sabemos tu nombre, anónimo.";
+    }else{
         txt = "¡A disfrutar del juego, " + person + "!";
     }
     document.getElementById("nombre").innerHTML = txt;
 }
+welcome();
 
-saludo()
+var tiles = ["img/captain.png", "img/captain.png", "img/hulk.png", "img/hulk.png", "img/hawkeye.png", 
+              "img/hawkeye.png", "img/thor.png", "img/thor.png", "img/blackwidow.png",
+              "img/blackwidow.png", "img/ironman.png", "img/ironman.png"];
+var cont = 0;
+var tries = 0;
+var pairs = 0;
+var randomizado = random(tiles);
 
-// 2- Deben acomodarse las fichas de forma aleatoria, 
-// cada vez que se inicie un nuevo juego.[x]
-
-$('.ada-front').on('click', function(){
-
-        var indice = 0;
-        img = new Array();
-        img[0] = 'img/feminist.jpg';
-        img[1] = 'img/empowered.jpg';
-        img[2] = 'img/feminist.jpg';
-        img[3] = 'img/adaLovelace.jpg';
-        img[4] = 'img/empowered.jpg';
-        img[5] = 'img/adaLovelace.jpg';
-        img[6] = 'img/chienS.png';
-        img[7] = 'img/fundamental.png';
-        img[8] = 'img/graceHopper.jpg';
-        img[9] = 'img/graceHopper.jpg';
-        img[10] = 'img/chienS.png';
-        img[11] = 'img/fundamental.png';
-
-        indice = Math.random() * (img.length);
-        indice = Math.floor(indice);
-        for (i=1;i<=img.length;i++) {
-            if (indice == img.length) indice = 0;
-            img.src=img[2];
-            indice++;
-        }
-});
-
-// 3- Al completar todos los pares mostrar un mensaje de ganÛ. 
-// 5- Tener 24 oportunidades, si no descubre todo el tablero en esa cantidad perder·.
-
-function turnTiles(fichas){
-
+function random(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+  return array;
 }
 
+for(var i = 0; i < randomizado.length; i++) {
+    $(".ficha").eq(i).children("img").attr("data-turn", randomizado[i]);
+}
 
+var tile = {
+                data: "",
+                id: null
+};
 
+$("img").on("click", function() {
+    if(cont < 2 && tile.id != $(this)[0].id && !$(this).hasClass("disabled")){
+        $(this).attr("src", $(this).data("turn")); 
+        if(tile.data == "") {
+            tile.data = $(this).data("turn");
+            tile.id = $(this)[0].id;
+            cont++;
+        }else {
+            if(tile.data == $(this).data("turn")) {
+                $("#" + tile.id).addClass("disabled");
+                $(this).addClass("disabled");
+                tile = {
+                        data: "",
+                        id: null
+                };
+                cont = 0;
+                tries++;
+                $(".tries").html(tries);
+                pairs++;
+            }else {
+                const that = $(this);
+                setTimeout(function() {
+                    that.attr("src", "img/card.png");
+                    $("#" + tile.id).attr("src", "img/card.png");
+                    tile = {
+                        data: "",
+                        id: null
+                    };
+                  cont = 0;
+                }, 500);
+                cont++;
+                tries++;
+                $(".tries").html(tries);
+            }
+        }
+    }
+    if(tries < 24 && pairs == 6) {
+        $(".won").removeClass("hide");
+    }else if(tries == 24) {
+        $(".lose").removeClass("hide");
+        $('img').unbind("click");
+    }
+});
 
-
+$(".restart").on("click", function() {
+  window.location.reload(true);
+});
